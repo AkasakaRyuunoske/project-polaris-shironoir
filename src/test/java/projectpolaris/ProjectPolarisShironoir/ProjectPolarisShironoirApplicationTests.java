3738135b46f35d17bc0495396c130c75c365e778
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-02 21:40:36 +0200

    First commit

---
+++ src/test/java/projectpolaris/ProjectPolarisShironoir/ProjectPolarisShironoirApplicationTests.java
@@ -0,0 +1,13 @@
+package projectpolaris.ProjectPolarisShironoir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPolarisShironoirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
